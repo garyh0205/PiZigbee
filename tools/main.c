497e93cf16b6a2c809992fab484e34354d5ad4a0
garyh0205 <26973371+garyh0205@users.noreply.github.com>
2017-12-29 02:27:59 +0800

    Add a connection to Local for getting Local Address
    In order to get 16-bit adddress from Zigbee. And create a variable "Local Address" to store the address of Zigbee.

--- tools/main.c
+++ tools/main.c
@@ -9,40 +9,56 @@ int main(void) {
 	struct xbee *xbee;
 	struct xbee_con *con;
 	struct xbee_conAddress address;
-	xbee_err ret;
 	struct xbee_conSettings settings;
-	
-	/* 
-	 * xbee_setup(struct xbee **retXbee, const char *mode, char *device, int baudrate) 
-	 *
-	 */
+	xbee_err ret;
+	unsigned char txRet;
+
+	/*-----------------------------Configuration for xbee---------------------------------*/
+	/*------------------------------------------------------------------------------------*/
+
+	/* xbee_setup(struct xbee **retXbee, const char *mode, char *device, int baudrate) */
 	if ((ret = xbee_setup(&xbee, mode, device, baudrate)) != XBEE_ENONE) {
 		printf("ret: %d (%s)\n", ret, xbee_errorToStr(ret));
 		return ret;
 	}
-	
-	/* Setup Log Level */
+
+	/* Setup Log Level 0:disable Log, 100:enable Log */
 	if ((ret = xbee_logLevelSet(xbee,LogLevel)) != XBEE_ENONE) {
 		printf("ret: %d (%s)\n", ret, xbee_errorToStr(ret));
 		return ret;
 	}
 	
-	/* Setup a adrress */
+	/*----------------------Configuration for connection in AT mode-----------------------*/
+	/* In this mode we aim to get the address of xbee.                                    */
+	/*------------------------------------------------------------------------------------*/
+	
+	/* Setup a default adrress. 
+	 * If it is a coordinator, it might be brocast.
+	 * If it is a router, it might be connect to coordinator as default.
+	 */
 	memset(&address, 0, sizeof(address));
 	address.addr64_enabled = 1;
 	if(Brocast == 1)
 		AddressToChar("000000000000FFFF",address.addr64);
-		//sscanf("000000000000FFFF", "%2x%2x%2x%2x%2x%2x%2x%2x", &address.addr64[0], &address.addr64[1], &address.addr64[2], &address.addr64[3],
-		//										 				 &address.addr64[4], &address.addr64[5], &address.addr64[6], &address.addr64[7]   );
 
-	if ((ret = xbee_conNew(xbee, &con, "Data", &address)) != XBEE_ENONE) {
+	/* create a new AT connection to the local XBee in order to get the address of the xbee. */
+	if ((ret = xbee_conNew(xbee, &con, "Local AT", NULL)) != XBEE_ENONE) {
 		xbee_log(xbee, -1, "xbee_conNew() returned: %d (%s)", ret, xbee_errorToStr(ret));
+		return ret;
+	}
+	
+	/*
+	 * configure a callback for the connection of AT mode
+	 * this function is called every time a packet for this connection is recieved 
+	 */
+	if ((ret = xbee_conCallbackSet(con, myCB_AT, NULL)) != XBEE_ENONE) {
+		xbee_log(xbee, -1, "xbee_conCallbackSet() returned: %d", ret);
 		return ret;
 	}
 	
 	/* if settings.catchAll = 1, then all packets will receive */
 	if (xbee_conSettings(con, NULL, &settings) != XBEE_ENONE) return ret; 
-	settings.catchAll = 1; 
+	settings.catchAll = 0; 
 	
 	if (xbee_conSettings(con, &settings, NULL) != XBEE_ENONE) return ret;
 	
@@ -51,12 +67,55 @@ int main(void) {
 		return ret;
 	}
 	
-	/* Set CallBack Function to call myCB if packet receive. */
-	if ((ret = xbee_conCallbackSet(con, myCB, NULL)) != XBEE_ENONE) {
+	Local_Address = malloc(sizeof(char)*8);
+	/* send the AT command 'SH','SL' */
+	xbee_conTx(con, &txRet, "SH");
+	printf("SH sended\n");
+	usleep(500000);
+	while(get_address != 0){
+		if(get_address == 2){
+			xbee_conTx(con, &txRet, "SL");
+			printf("SL sended\n");
+			get_address = 1;
+		}
+		usleep(500000);
+	}
+	
+	/* shutdown the connection */
+	if ((ret = xbee_conEnd(con)) != XBEE_ENONE) {
+		xbee_log(xbee, -1, "xbee_conEnd() returned: %d", ret);
+		return ret;
+	}
+	
+	printf("The Address of this device is %s",Local_Address);
+
+	/*--------------------Configuration for connection in Data mode-----------------------*/
+	/* In this mode we aim to get Data.                                                   */
+	/*------------------------------------------------------------------------------------*/
+	if ((ret = xbee_conNew(xbee, &con, "Data", &address)) != XBEE_ENONE) {
+		xbee_log(xbee, -1, "xbee_conNew() returned: %d (%s)", ret, xbee_errorToStr(ret));
+		return ret;
+	}
+	
+	/* Set CallBack Function to call myCB_Data if packet receive. */
+	if ((ret = xbee_conCallbackSet(con, myCB_Data, NULL)) != XBEE_ENONE) {
 		xbee_log(xbee, -1, "xbee_conCallbackSet() returned: %d", ret);
 		return ret;
 	}
-	init_Queue(); 
+	
+	/* if settings.catchAll = 1, then all packets will receive */
+	if (xbee_conSettings(con, NULL, &settings) != XBEE_ENONE) return ret; 
+	settings.catchAll = 1; 
+
+	if (xbee_conSettings(con, &settings, NULL) != XBEE_ENONE) return ret;
+	
+	if ((ret = xbee_conDataSet(con, xbee, NULL)) != XBEE_ENONE) {
+		xbee_log(xbee, -1, "xbee_conDataSet() returned: %d", ret);
+		return ret;
+	}
+	
+	init_Queue();
+	
 	/* start the chain reaction! */
 	while(1) {
 		void *p;
@@ -71,8 +130,8 @@ int main(void) {
 		usleep(500000);
 		if(front->next != NULL){
 			printf("%s\n",front->next->content);
-			//xbee_conTx(con, NULL, send_data);
-			//usleep(5000000);
+			xbee_conTx(con, NULL, front->next->content);
+			usleep(5000000);
 			delq();
 		}
 		usleep(500000);
@@ -91,8 +150,38 @@ int main(void) {
 }
 
 void AddressToChar(char *raw,unsigned char addr[8]){
+
 	sscanf(raw, "%2x%2x%2x%2x%2x%2x%2x%2x", &addr[0], &addr[1], &addr[2], &addr[3],
 											&addr[4], &addr[5], &addr[6], &addr[7]   );
+}
+
+void AddressCopyHalf(char *raw,char *dest){
+	memcpy(dest,raw,4);
+	printf("raw Address Copy to dest Address\n");
+	printf("raw: %2x%2x%2x%2x\n", raw[0], raw[1], raw[2], raw[3]);
+	printf("dest: %2x%2x%2x%2x\n", dest[0], dest[1], dest[2], dest[3]);
+}
+
+/* 
+ * this is the callback function...
+ * it will be executed once for each packet that is recieved on an associated connection 
+ */
+ //A flag for check if all part of address are get. 2 send first address 1 send second address 0 success
+void myCB_AT(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data) {
+	if ((*pkt)->dataLen == 0) {
+		printf("too short...\n");
+		return;
+	}
+	if(strcmp(((*pkt)->conType),("Local AT"))==0){
+		if(get_address == 3){
+			AddressCopyHalf(((*pkt)->data),Local_Address);
+			get_address = 2;
+		}else if(get_address == 1){
+			AddressCopyHalf(((*pkt)->data),&Local_Address[4]);
+			printf("Local_Address: %2x%2x%2x%2x%2x%2x%2x%2x\n", Local_Address[0], Local_Address[1], Local_Address[2], Local_Address[3], Local_Address[4], Local_Address[5], Local_Address[6], Local_Address[7]);
+			get_address = 0;
+		}
+	}
 }
 
 /* 
@@ -100,7 +189,7 @@ void AddressToChar(char *raw,unsigned char addr[8]){
  * it will be executed once for each packet that is recieved on an associated connection 
  * if settings.catchAll = 1 then it will be executed for every packet.
  */
-void myCB(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data) {
+void myCB_Data(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data) {
 	if ((*pkt)->dataLen > 0) {
 		/* if data[0] == '@', callback will be end. */ 
 		if ((*pkt)->data[0] == '@') {
@@ -117,7 +206,7 @@ void myCB(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void *
 	}
 	/* printf("tx: %d\n", xbee_conTx(con, NULL, "Succcess\r\n")); */
 }
-
+	
 void init_Queue() { 
     front = rear = (pPkt*) malloc(sizeof(pPkt)); 
     front->next = rear->next = NULL; 
@@ -153,4 +242,3 @@ void delq() {
     front->next = tmpnode->next; 
     free(tmpnode);
 } 
-
--- tools/setting.h
+++ tools/setting.h
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <xbee.h>
 
 //For xbee_setup
-extern const char *mode = "xbeeZB";
-extern char *device = "/dev/ttyAMA0";
-extern int baudrate = 9600;
-extern int LogLevel =  100; //0:disable Log, 100:enable Log
-extern int Brocast = 1; //0:For Brocast or Coordinator, 1:Send to cooedinator
-
-
- /* queue for sending data */
+const char *mode = "xbeeZB";
+char *device = "/dev/ttyAMA0";
+int baudrate = 9600;
+int LogLevel =  0; //0:disable Log, 100:enable Log
+int Brocast = 1; 	//0:For Brocast or a Coordinator, 1:Send to cooedinator or a endpoint
+int get_address = 3; //A flag for check if all part of address are get. 3 send first address 2 send second address 0 success
+char* Local_Address = "";
+/* queue for sending data */
 struct pkt { 
 	char *type;                 //"Data" or "AT"
 	unsigned char address[8];   //Brocast:000000000000FFFF;Coordinator:0000000000000000
@@ -26,4 +25,6 @@ void init_Queue();
 void addpkt(char *raw_addr, char *type,char *content); 
 void delq();
 void AddressToChar(char *raw,unsigned char addr[8]);
-void myCB(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data);
+void AddressCopyHalf(char *raw,char *dest);
+void myCB_Data(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data);
+void myCB_AT(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data);
